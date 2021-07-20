c0ae9778066c8655e23c52348d58e885c3685d87
zgbartol <zgbartol@hotmail.com>
2021-07-20 10:16:56 -0300

    agrego namespace

---
+++ cesar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    int li = 3;
+    do {
+         cout << endl;
+    } while (li-- >0);
+    while (entrada != " "){
+        cout << "Ingrese frase a encriptar" << endl;
+        cin >> entrada;
+        //MIENTRAS (haya caracteres en la entrada) {
+            // extraigo un caracter de entrada
+            // lo sustituyo por un caracter + 4
+            // lo ingreso a la salida
+            int largocadena = 
+
+    }
+	
+    return 0;
+}
