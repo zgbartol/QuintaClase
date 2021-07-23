9d36f2c7f12702402b50250db27fa2a67549ac97
zgbartol <zgbartol@hotmail.com>
2021-07-23 11:00:24 -0300

    cesar terminado con comentarios

--- loops.cpp
+++ loops.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
     double diametro {1};
     constexpr double pi=3.14159265;
     cout  << endl;
+    char mensajeHabitual [38] ="Ingrese el diámetro, 0 para salir  ";
+    char agradeceporUsar [38] {"Gracias por usar este programa "};
+    char mensajeResultado [38] {"El largo de la circunferencia es "};
     do
     {
-        cout << endl << "Ingrese el diámetro, 0 para salir  ";
+        cout << endl << mensajeHabitual;
         cin >> diametro;
         if (!diametro) {
-            cout << endl << "Gracias por usar este programa "<< endl;
+            cout << endl <<agradeceporUsar << endl;
         } else {
-        cout << endl << "El largo de la circunferencia es "<< diametro * pi << endl;
+        cout << endl << mensajeResultado << diametro * pi << endl;
         }
     } while(diametro);
 	return 0;
